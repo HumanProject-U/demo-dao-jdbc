9cddb58bdf1765a06f40c67f7cac807c35fa481a
Christian De Oliveira <pokeracposterhuman@gmail.com>
2020-01-15 23:31:34 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,12 +21,13 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
+      //  Department obj = new Department(1, "Books");
         
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+     //   Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
         
        // System.out.println(obj);
        SellerDao sellerDao = DaoFactory.createSellerDao();
+       Seller seller = sellerDao.findById(3);
        
         System.out.println(seller);
     }
