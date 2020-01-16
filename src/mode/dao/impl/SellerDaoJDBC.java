01d942879a7496a588793969396990ddd6c306b9
Christian De Oliveira <pokeracposterhuman@gmail.com>
2020-01-15 21:31:37 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/mode/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mode.dao.impl;
+
+import java.util.List;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+/**
+ *
+ * @author Christian
+ */
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(Seller obj) {
+    }
+
+    @Override
+    public void update(Seller obj) {
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        
+        return null;
+    }   
+}
